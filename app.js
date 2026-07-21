448982a5590424995fe6f7ef51e5bf6c1d4f5859
Tina <contactina101@gmail.com>
2026-07-21 21:43:13 +0545

    Handle CLI commands

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
 const command = process.argv[2];
 const argument = process.argv[3];
 
-console.log("Command:", command);
-console.log("Argument:", argument);
+if (command === "add") {
+    console.log(`Adding task: ${argument}`);
+} else if (command === "list") {
+    console.log("Listing all tasks...");
+} else if (command === "delete") {
+    console.log(`Deleting task ${argument}`);
+} else {
+    console.log("Unknown command.");
+}
